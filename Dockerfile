af1ad9f6b66f610973db5dfe240a5f94c3186f13
Joshua Sierles <joshua@fly.io>
2022-02-20 12:09:30 +0100

    Run rsyncd for receiving build contents

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM nixos/nix
 RUN nix-env -i mount curl rsync ps flyctl skopeo
 COPY boot.sh /boot.sh
+COPY rsyncd.conf /etc/rsyncd.conf
 CMD /boot.sh
--- boot.sh
+++ boot.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-echo "Syncing nix store to the persistent volume..."
-rsync -av /nix/* /nix_store
+mkdir -p ${DATA_DIR}/source
+echo "Syncing nix store to the persistent volume."
+rsync -av /nix/* ${DATA_DIR}/nix_store
+echo "Mounting nix store to /nix."
 mount --bind /nix_store /nix
-tail -f /dev/null
+echo "Starting rsync daemon."
+rsync --daemon --no-detach
